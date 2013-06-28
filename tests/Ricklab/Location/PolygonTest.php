d77102f30997bebe7f81355645686fb4332f9d3e
Rick Ogden <rick@rickogden.com>
2013-06-28 12:16:11 +0100

    sorted formatting and removed error reporting.

--- tests/Ricklab/Location/PolygonTest.php
+++ tests/Ricklab/Location/PolygonTest.php
@@ -2,40 +2,45 @@
 
 namespace Ricklab\Location\Tests;
 
-require __DIR__.'/../../../vendor/autoload.php';
+require __DIR__ . '/../../../vendor/autoload.php';
 
 use \Ricklab\Location;
-error_reporting(E_ALL);
-class PolygonTest extends \PHPUnit_Framework_TestCase {
+
+class PolygonTest extends \PHPUnit_Framework_TestCase
+{
+
     /**
      *
      * @var Ricklab\Location\Polygon 
      */
     public $polygon;
-    
-    public function setUp() {
-        $this->polygon = new Location\Polygon([new Location\Point(2, 3), new Location\Point(2, 4), new Location\Point(3, 4)]);
 
+    public function setUp()
+    {
+        $this->polygon = new Location\Polygon([new Location\Point(2, 3), new Location\Point(2, 4), new Location\Point(3, 4)]);
     }
-    
-    public function testLastPointIsTheSameAsFirstPoint() {
+
+    public function testLastPointIsTheSameAsFirstPoint()
+    {
         $a = $this->polygon->toArray();
         $this->assertEquals($a[0]->getLatitude(), $a[count($a) - 1]->getLatitude());
         $this->assertEquals($a[0]->getLongitude(), $a[count($a) - 1]->getLongitude());
-   }
-    public function testToArrayReturnsAnArray() {
+    }
+
+    public function testToArrayReturnsAnArray()
+    {
         $this->assertTrue(is_array($this->polygon->toArray()));
-   }
-    
-    
-    public function testObjectIsAPolygon() {
-        
+    }
+
+    public function testObjectIsAPolygon()
+    {
+
         $this->assertInstanceOf('Ricklab\Location\Polygon', $this->polygon);
     }
-    
+
     public function tearDown()
     {
         $this->polygon = null;
     }
-    
+
 }
