5f8a24cafa67f2542f1728bc48c48c451fe4edce
Rick Ogden <rick@rickogden.com>
2015-05-25 11:26:31 +0100

    Altered unit conversion. Now all conversion is done from kilometres.

--- src/Ricklab/Location/Earth.php
+++ src/Ricklab/Location/Earth.php
@@ -11,20 +11,5 @@ namespace Ricklab\Location;
 class Earth extends Planet
 {
 
-    protected static $radius = array(
-        'km' => 6371.009,
-        'miles' => 3958.761,
-        'm' => 6371009,
-        'nmi' => 3440.069
-    );
-
-    public function radius($unit = 'km', $location = null)
-    {
-        if (isset(self::$radius[$unit])) {
-            return self::$radius[$unit];
-        } else {
-            throw new \InvalidArgumentException('Argument is not a valid unit');
-        }
-    }
-
+    protected $radius = 6371.009;
 }
