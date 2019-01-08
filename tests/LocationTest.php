1e152723ef1a4112830d583ec1422aa5a191ec33
Rick Ogden <rick@airtimerewards.com>
2019-01-08 09:58:15 +0000

    Added test for without the geospatial extension on final bearing.

--- tests/LocationTest.php
+++ tests/LocationTest.php
@@ -118,6 +118,10 @@ class LocationTest extends TestCase
     {
         $point1 = new Point(0.119, 52.205);
         $point2 = new Point(2.351, 48.857);
+        Location::$useSpatialExtension = false;
+        $finalBearing = Location::getFinalBearing($point1, $point2);
+        $this->assertSame(157.9, \round($finalBearing, 1));
+        Location::$useSpatialExtension = true;
         $finalBearing = Location::getFinalBearing($point1, $point2);
         $this->assertSame(157.9, \round($finalBearing, 1));
     }
