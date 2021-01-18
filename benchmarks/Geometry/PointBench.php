69bc3f1c37ecbe68edd36ede1828f2130192312b
Rick Ogden <rick@airtimerewards.com>
2021-01-18 17:34:54 +0000

    Updated benchmarks.

--- benchmarks/Geometry/PointBench.php
+++ benchmarks/Geometry/PointBench.php
@@ -2,8 +2,13 @@
 
 declare(strict_types=1);
 
+use Ricklab\Location\Calculator\BearingCalculator;
+use Ricklab\Location\Calculator\DistanceCalculator;
+use Ricklab\Location\Calculator\EquirectangleCalculator;
+use Ricklab\Location\Calculator\HaversineCalculator;
+use Ricklab\Location\Calculator\VincentyCalculator;
+use Ricklab\Location\Converter\UnitConverter;
 use Ricklab\Location\Geometry\Point;
-use Ricklab\Location\Location;
 
 /**
  * @BeforeMethods({"initPoints"})
@@ -20,16 +25,19 @@ class PointBench
 
     public function enableGeospatialExtension(): void
     {
-        Location::$useSpatialExtension = true;
+        HaversineCalculator::enableGeoSpatialExtension();
+        VincentyCalculator::enableGeoSpatialExtension();
+        BearingCalculator::enableGeoSpatialExtension();
     }
-
 
     /**
      * @BeforeMethods({"disableGeospatialExtension", "initPoints"})
      */
     public function disableGeospatialExtension(): void
     {
-        Location::$useSpatialExtension = false;
+        HaversineCalculator::disableGeoSpatialExtension();
+        VincentyCalculator::disableGeoSpatialExtension();
+        BearingCalculator::disableGeoSpatialExtension();
     }
 
     public function initPoints(): void
@@ -42,20 +50,20 @@ class PointBench
         ];
     }
 
-    public function runDistanceTo(int $formula = Location::FORMULA_HAVERSINE): void
+    public function runDistanceTo(DistanceCalculator $formula): void
     {
-        $this->points[0]->distanceTo($this->points[1], Location::UNIT_METRES, $formula);
-        $this->points[0]->distanceTo($this->points[2], Location::UNIT_METRES, $formula);
-        $this->points[0]->distanceTo($this->points[3], Location::UNIT_METRES, $formula);
-        $this->points[1]->distanceTo($this->points[0], Location::UNIT_METRES, $formula);
-        $this->points[1]->distanceTo($this->points[2], Location::UNIT_METRES, $formula);
-        $this->points[1]->distanceTo($this->points[3], Location::UNIT_METRES, $formula);
-        $this->points[2]->distanceTo($this->points[0], Location::UNIT_METRES, $formula);
-        $this->points[2]->distanceTo($this->points[1], Location::UNIT_METRES, $formula);
-        $this->points[2]->distanceTo($this->points[3], Location::UNIT_METRES, $formula);
-        $this->points[3]->distanceTo($this->points[0], Location::UNIT_METRES, $formula);
-        $this->points[3]->distanceTo($this->points[1], Location::UNIT_METRES, $formula);
-        $this->points[3]->distanceTo($this->points[2], Location::UNIT_METRES, $formula);
+        $this->points[0]->distanceTo($this->points[1], UnitConverter::UNIT_METERS, $formula);
+        $this->points[0]->distanceTo($this->points[2], UnitConverter::UNIT_METERS, $formula);
+        $this->points[0]->distanceTo($this->points[3], UnitConverter::UNIT_METERS, $formula);
+        $this->points[1]->distanceTo($this->points[0], UnitConverter::UNIT_METERS, $formula);
+        $this->points[1]->distanceTo($this->points[2], UnitConverter::UNIT_METERS, $formula);
+        $this->points[1]->distanceTo($this->points[3], UnitConverter::UNIT_METERS, $formula);
+        $this->points[2]->distanceTo($this->points[0], UnitConverter::UNIT_METERS, $formula);
+        $this->points[2]->distanceTo($this->points[1], UnitConverter::UNIT_METERS, $formula);
+        $this->points[2]->distanceTo($this->points[3], UnitConverter::UNIT_METERS, $formula);
+        $this->points[3]->distanceTo($this->points[0], UnitConverter::UNIT_METERS, $formula);
+        $this->points[3]->distanceTo($this->points[1], UnitConverter::UNIT_METERS, $formula);
+        $this->points[3]->distanceTo($this->points[2], UnitConverter::UNIT_METERS, $formula);
     }
 
     public function runBearingTo(): void
@@ -79,7 +87,7 @@ class PointBench
      */
     public function benchDistanceToHaversineWithSpatialExtension(): void
     {
-        $this->runDistanceTo();
+        $this->runDistanceTo(new HaversineCalculator());
     }
 
     /**
@@ -87,23 +95,26 @@ class PointBench
      */
     public function benchDistanceToVincentyWithSpatialExtension(): void
     {
-        $this->runDistanceTo(Location::FORMULA_VINCENTY);
+        $this->runDistanceTo(new VincentyCalculator());
     }
 
     public function benchDistanceToHaversine(): void
     {
-        $this->runDistanceTo();
+        $this->runDistanceTo(new HaversineCalculator());
     }
 
+    public function benchDistanceEquirectangle(): void
+    {
+        $this->runDistanceTo(new EquirectangleCalculator());
+    }
 
     /**
      * @BeforeMethods({"disableGeospatialExtension", "initPoints"})
      */
     public function benchDistanceToVincenty(): void
     {
-        $this->runDistanceTo(Location::FORMULA_VINCENTY);
+        $this->runDistanceTo(new VincentyCalculator());
     }
-
 
     /**
      * @BeforeMethods({"disableGeospatialExtension", "initPoints"})
